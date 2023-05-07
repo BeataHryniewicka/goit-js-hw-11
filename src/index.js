487bd1275252dda8151d9d4bd8b13b0817c4bb7f
BeataHryniewicka <118068394+BeataHryniewicka@users.noreply.github.com>
2023-05-07 23:28:54 +0200

    .

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-var debounce = require('lodash.debounce');
+//var debounce = require('lodash.debounce');
 
 const inputEl = document.querySelector('input[name="searchQuery"]');
-const keyWord = inputEl.value;
 const btnSearch = document.querySelector('.search-btn');
 const btnLoadMore = document.querySelector('.load-more');
-const galeryListEl = document.querySelector('.galleryList');
+const galleryEl = document.querySelector('.gallery');
 
 const API_URL = `https://pixabay.com/api/`;
 const API_KEY = `36096089-8019a6978013fb7a12ca287ee`;
 
-const defaultImgPerPage = 40;
+const defaultImgPerPage = 3;
 const page = 1;
+
 async function fetchImages() {
   try {
+    let keyWord = inputEl.value;
     const response = await axios.get(
       `${API_URL}?key=${API_KEY}&q=${keyWord}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${defaultImgPerPage}`
     );
@@ -25,10 +26,10 @@ async function fetchImages() {
         'Sorry, there are no images matching your search query. Please try again.'
       );
     }
-
     console.log(response.data.hits);
     console.log(response.data.totalHits);
     console.log(keyWord);
+
     const totalHits = response.data.totalHits;
     if (totalHits > 0)
       Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
@@ -37,11 +38,12 @@ async function fetchImages() {
     console.error('Error:' + error);
   }
 }
-//btnSearch.addEventListener('click', fetchImages);
 
-async function createImg() {
+async function createImages() {
   //tablica obiektów
   const newImages = await fetchImages();
+  btnLoadMore.style.display = 'block';
+
   //tablica ciągów html
   const imagesHTML = newImages
     //używamy metody map() na tablicy newImages, aby utworzyć nową tablicę imagesHTML, która składa się z ciągów HTML reprezentujących każdy obrazek.
@@ -61,26 +63,28 @@ async function createImg() {
     )
     .join(' ');
 
-  galeryListEl.innerHTML += imagesHTML;
+  galleryEl.innerHTML += imagesHTML;
   new SimpleLightbox('.gallery a');
 }
-function writeKeyWord() {
-  // e.preventDefault();
-  const keyWord = inputEl.value;
-  console.log(keyWord);
-  createImg();
+
+btnSearch.addEventListener('click', createImages);
+
+function loadMore() {
+  console.log("We're sorry, but you've reached the end of search results.");
+  // page++;
+  btnLoadMore.display.style = 'none';
+  Notiflix.Notify.info(
+    "We're sorry, but you've reached the end of search results."
+  );
 }
+btnLoadMore.addEventListener('click', loadMore);
 
-inputEl.addEventListener(
-  'input',
-  debounce(
-    //musi sie wydarzyc zdarzenie, które działa po kliknieciu w button search
-    writeKeyWord,
-    1500
-  )
-);
-
-//btnLoadMore.addEventListener('click', createImg);
+//do zrobienia:
+//paginacja
+//buttony chowanie
+//załadowanie kolejnej strony
+//We're sorry, but you've reached the end of search results."
+//simplelightbox - nie działa
 
 // function createApiObjects() {
 //   new apiObject({
