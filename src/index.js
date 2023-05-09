959c44d76a46d056813e2d7b0470f3cd0b7b79af
BeataHryniewicka <118068394+BeataHryniewicka@users.noreply.github.com>
2023-05-09 23:09:21 +0200

    final

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 //var debounce = require('lodash.debounce');
-
+const formEl = document.querySelector('.search-form');
 const inputEl = document.querySelector('input[name="searchQuery"]');
 const btnSearch = document.querySelector('.search-btn');
 const btnLoadMore = document.querySelector('.load-more');
@@ -11,13 +11,14 @@ const galleryEl = document.querySelector('.gallery');
 
 const API_URL = `https://pixabay.com/api/`;
 const API_KEY = `36096089-8019a6978013fb7a12ca287ee`;
-const defaultImgPerPage = 16;
+
+let defaultImgPerPage = 4;
 let page = 1;
 
 //funkcja do pobrania danych z API
 async function fetchImages() {
   try {
-    const keyWord = inputEl.value.trim();
+    keyWord = inputEl.value.trim();
     const response = await axios.get(
       `${API_URL}?key=${API_KEY}&q=${keyWord}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${defaultImgPerPage}`
     );
@@ -30,12 +31,8 @@ async function fetchImages() {
 
 //funkcja do utworzenia nowej tablicy zdjęć na rezultacie z fetchImages, konwersja z tablicy obiektów na tablicę ciągów html, wepchniecie do html zdjęć z parametrami, podpięcie sie pod galerię SimpleLihtbox z paginacją
 async function createImages() {
-  btnLoadMore.style.display = 'block';
   //tablica obiektów
   const newImages = await fetchImages();
-  if (newImages.totalHits <= defaultImgPerPage) {
-    btnLoadMore.style.display = 'none';
-  }
   //tablica ciągów html
   const imagesHTML = newImages.hits
     //console.log(imagesHTML);
@@ -57,37 +54,26 @@ async function createImages() {
 
   galleryEl.innerHTML += imagesHTML;
   new SimpleLightbox('.gallery a');
-
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
-
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
 }
 
 //funkcja createImages z powiadomieniami w zalezności od ilości zwrotów, potrzebna tylko w pierwszorazowym kliku na Show
 async function createImagesNotification() {
+  btnLoadMore.style.display = 'block';
   const newImages = await fetchImages();
-  // if ((inputEl.value === ' ')) {
-  //   alert('Please enter any word');
-  // }
   if (newImages.totalHits === 0) {
     Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
   } else {
     Notiflix.Notify.success(`Hooray! We found ${newImages.totalHits} images.`);
+    createImages();
   }
-  createImages();
+  galleryEl.innerHTML = '';
 }
 btnSearch.addEventListener('click', createImagesNotification);
 
 //funkcja ładowania kolejnych stron
 async function showNextPage() {
-  btnLoadMore.style.display = 'none';
   page++;
   const newImages = await fetchImages();
   if (page > newImages.totalHits / defaultImgPerPage + 1) {
@@ -95,9 +81,6 @@ async function showNextPage() {
       "We're sorry, but you've reached the end of search results."
     );
   }
-  const nextPage = createImages();
-  nextPage.refresh();
-
   const { height: cardHeight } = document
     .querySelector('.gallery')
     .firstElementChild.getBoundingClientRect();
@@ -106,10 +89,10 @@ async function showNextPage() {
     top: cardHeight * 2,
     behavior: 'smooth',
   });
+  createImages();
 }
 
-btnLoadMore.addEventListener('click', e => {
-  e.preventDefault();
+btnLoadMore.addEventListener('click', () => {
   btnLoadMore.style.display = 'none';
   showNextPage();
   btnLoadMore.style.display = 'block';
